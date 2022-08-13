44d6d790d3429318a4638d20d8e905ed6ddae8d3
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-13 10:20:09 +0530

    added the code to check leap year in LeapYearChecker.java

--- src/main/java/io/github/dbc/LeapYearChecker.java
+++ src/main/java/io/github/dbc/LeapYearChecker.java
@@ -8,6 +8,9 @@ public class LeapYearChecker {
 	 * @return true if the year is a leap year, false otherwise.
 	 */
 	public boolean isLeapYear(int year) {
+		if (((year%4==0) && (year%100 != 0)) || (year%400 == 0)) {
+			return  true;
+		}
 		return false;
 	}
 }
