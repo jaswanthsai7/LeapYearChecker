2b51486a7c6e645958899786644a11bc0ba6af7a
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-13 10:20:32 +0530

    final commit

--- src/main/java/io/github/dbc/LeapYearChecker.java
+++ src/main/java/io/github/dbc/LeapYearChecker.java
@@ -8,7 +8,8 @@ public class LeapYearChecker {
 	 * @return true if the year is a leap year, false otherwise.
 	 */
 	public boolean isLeapYear(int year) {
-		if (((year%4==0) && (year%100 != 0)) || (year%400 == 0)) {
+		if (((year%4==0) && (year%100 != 0)) || (year%400 == 0))
+		{
 			return  true;
 		}
 		return false;
